a58abd050ed3aeb30c66337d81559ee2c9ea43ca
FrederikJ <fredrik_johnsen92@hotmail.com>
2015-04-07 16:10:56 +0200

    lagt inn db connection i repository

--- HovedOppgave/HovedOppgave/Classes/Repository.cs
+++ HovedOppgave/HovedOppgave/Classes/Repository.cs
@@ -5,22 +5,66 @@ using System.Linq;
 using System.Web;
 using MySql.Data.MySqlClient;
 
+//for info om skrive query - http://www.codeproject.com/Articles/43438/Connect-C-to-MySQL
+
 namespace HovedOppgave.Models
 {
     public class Repository : IRepository
     {
         MySqlConnection conn;
+        HttpContext http = HttpContext.Current;
 
         public Repository()
         {
-            conn = new MySqlConnection();
+            this.Initialize();    
+        }
+
+        #region DB klasser
+        private void Initialize()
+        {
             string myConnectionString = "server=82.164.4.64;user id=boppg;persistsecurityinfo=True;database=mydb;port=3300";
+            conn = new MySqlConnection(myConnectionString);  
+        }
+        private bool OpenConnection()
+        {
             try
             {
-                conn,
+                conn.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //To meste vanlige problemene
+                switch (ex.Number)
+                {
+                    case 0:
+                        http.Session["flashMessage"] = "Kunne ikke åpne forbindelse";
+                        http.Session["flashStatus"] = Constants.NotificationType.danger.ToString();
+                        break;
+                    case 1045:
+                        http.Session["flashMessage"] = "Feil brukernavn/passord, prøv igjen";
+                        http.Session["flashStatus"] = Constants.NotificationType.danger.ToString();
+                        break;
+                }
+                return false;
             }
         }
-        
+        private bool CloseConnection()
+        {
+            try
+            {
+                conn.Close();
+                return true;
+            }
+            catch(MySqlException ex)
+            {
+                http.Session["flashMessage"] = "Kunne ikke stenge forbindelse - " + ex.Message;
+                http.Session["flashStatus"] = Constants.NotificationType.danger.ToString();
+                return false;
+            }
+        }
+        #endregion
+
         #region Building Queries
         #endregion
         #region Cable Queries
